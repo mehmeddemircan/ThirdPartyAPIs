432aa8379563f6904c998e318a537434e0e8143b
mehmet demircan <mehmeddemircan@gmail.com>
2024-08-21 13:24:52 +0300

    added newyork times popular news service & controller

---
+++ src/Business/NewyorkTimes/NewyorkTimesService.cs
@@ -0,0 +1,32 @@
+﻿using Core.APIConfig;
+using Microsoft.Extensions.Options;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.NewyorkTimes
+{
+    public class NewyorkTimesService : INewyorkTimesService
+    {
+        private readonly RestClient _client;
+        private readonly string _apiKey;
+
+        public NewyorkTimesService(IOptions<ApiSettings> apiSettings)
+        {
+            _client = new RestClient(apiSettings.Value.BaseUrl);
+            _apiKey = apiSettings.Value.ApiKey;
+        }
+
+        public async Task<string> GetMostPopularAsync()
+        {
+            var request = new RestRequest("emailed/7.json", Method.Get);
+            request.AddQueryParameter("api-key", _apiKey);
+
+            var response = await _client.ExecuteAsync(request);
+            return response.Content;
+        }
+    }
+}
---
+++ src/newyorkTimes/NewyorkTimes.API/Controllers/NewyorkTimesController.cs
@@ -0,0 +1,32 @@
+﻿using Business.NewyorkTimes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewyorkTimes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewyorkTimesController : ControllerBase
+    {
+
+        private readonly INewyorkTimesService _newyorkTimesService;
+
+        public NewyorkTimesController(INewyorkTimesService newyorkTimesService)
+        {
+            _newyorkTimesService = newyorkTimesService;
+        }
+
+
+        /// <summary>
+        ///  Bütün Kur birimleri
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet("mostPopular")]
+        public async Task<IActionResult> GetMostPopularNewyorkTimes()
+        {
+            var result = await _newyorkTimesService.GetMostPopularAsync();
+            return Ok(result);
+        }
+    }
+}
